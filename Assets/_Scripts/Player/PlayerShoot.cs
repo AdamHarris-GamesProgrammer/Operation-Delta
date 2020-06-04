0f28d041fce90878e0bfb2ac94d6f6fc91b93bde
Adam Harris <adam_harris@hotmail.com>
2020-06-04 15:30:31 +0100

    0x08: Implemented reloading and magazine size

--- Assets/_Scripts/Player/PlayerShoot.cs
+++ Assets/_Scripts/Player/PlayerShoot.cs
@@ -12,12 +12,18 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private float range = 100.0f;
     [Tooltip("Amount of bullets fired per second")]
     [SerializeField] private float fireRate = 5.0f;
+    [SerializeField] private int magazineSize = 24;
+    [Header("Reload Settings")]
+    private float reloadDuration = 2.5f;
+
+    float reloadTimer;
 
     float timeSinceLastShot;
     float timeNeededBetweenShots;
+    int currentClip;
 
-    //Magazine size
-    //Reload
+    bool reloading;
+
     //Recoil
     //Damage
     //Crosshair gets bigger and smaller depending on if you are zoomed in?
@@ -32,15 +38,28 @@ public class PlayerShoot : MonoBehaviour
     private void Start()
     {
         timeNeededBetweenShots = 1.0f / fireRate;
+        currentClip = magazineSize;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (reloading)
+        {
+            reloadTimer -= Time.deltaTime;
+            Debug.Log("Time until reload: " + reloadTimer);
+            if (reloadTimer <= 0.0f)
+            {
+                reloading = false;
+                currentClip = magazineSize;
+                Debug.Log("Reload Complete");
+            }
+        }
+
         timeSinceLastShot += Time.deltaTime;
         if (Input.GetMouseButton(0))
         {
-            if(timeSinceLastShot >= timeNeededBetweenShots)
+            if(timeSinceLastShot >= timeNeededBetweenShots && !reloading)
             {
                 Shoot();
             }
@@ -50,11 +69,17 @@ public class PlayerShoot : MonoBehaviour
     void Shoot()
     {
         timeSinceLastShot = 0.0f;
-
+        currentClip--;
         RaycastHit hit;
         if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range, layer))
         {
             Debug.Log(hit.transform.name);
+        }
+
+        if(currentClip == 0)
+        {
+            reloadTimer = reloadDuration;
+            reloading = true;
         }
     }
 }
