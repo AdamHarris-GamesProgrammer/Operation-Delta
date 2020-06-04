1e2831f4a4c6b0487e56e72435639399e49afa50
Adam Harris <adam_harris@hotmail.com>
2020-06-04 11:27:49 +0100

    0x05: Implemented basic sprint and crouch

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -9,12 +9,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpHeight = 3.0f;
     [SerializeField] private float gravityFactor = 2.0f;
 
+    [Header("Movement Speed Factors")]
+    [SerializeField] private float sprintSpeedFactor = 1.5f;
+    [SerializeField] private float crouchSpeedFactor = 0.5f;
+
     [Header("Ground Settings")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] public LayerMask groundMask;
 
+
+    [Header("Camera")]
+    [SerializeField] private float yOffsetOnCrouch = -1.25f;
+    private Transform cameraTransform;
+
+
+    private float speedFactor = 1.0f;
+
     bool isGrounded;
+    bool isCrouched = false;
+    bool isSprinting = false;
 
     Vector3 velocity;
 
@@ -23,11 +37,13 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Grounded Check
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         float yGravity = Physics.gravity.y * gravityFactor;
@@ -37,12 +53,55 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
+        //Crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouched = !isCrouched;
+
+            CrouchHeight();
+        }
+
+        //Sprinting
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+            if (isCrouched)
+            {
+                isCrouched = false;
+                CrouchHeight();
+            }
+        }
+        else
+        {
+            isSprinting = false;
+        }
+
+
+        //Movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        characterController.Move(move * speed * Time.deltaTime);
+
+        if (isCrouched)
+        {
+            speedFactor = crouchSpeedFactor;
+        }
+        else if (isSprinting)
+        {
+            speedFactor = sprintSpeedFactor;
+        }
+        else
+        {
+            speedFactor = 1.0f;
+        }
+
+        float finalSpeed = speed * speedFactor;
+        Debug.Log("Calculated Speed: " + finalSpeed);
+
+
+        characterController.Move(move * finalSpeed * Time.deltaTime);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -52,5 +111,17 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += yGravity * Time.deltaTime;
 
         characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void CrouchHeight()
+    {
+        if (isCrouched)
+        {
+            cameraTransform.position = new Vector3(cameraTransform.position.x, (cameraTransform.position.y + yOffsetOnCrouch), cameraTransform.position.z);
+        }
+        else
+        {
+            cameraTransform.position = new Vector3(cameraTransform.position.x, (cameraTransform.position.y - yOffsetOnCrouch), cameraTransform.position.z);
+        }
     }
 }
