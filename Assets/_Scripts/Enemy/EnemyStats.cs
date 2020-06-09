b00800be02e83e670b27943c34d4a84a752f9963
Adam Harris <adam_harris@hotmail.com>
2020-06-09 18:22:53 +0100

    0x15: Created base enemy stats class

---
+++ Assets/_Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Enemy/Stats File")]
+public class EnemyStats : ScriptableObject
+{
+    [Header("Health Settings")]
+    public float health = 100.0f;
+    [Header("Health Regeneration Settings")]
+    public bool healthRegenEnabled = false;
+    public float healthRegenRate = 2.5f;
+    public float regenerationCooldown = 5.0f;
+
+    [Header("Damage Settings")]
+    public float damage = 12.0f;
+    public float damageMultiplier = 1.0f;
+    public float attackRate = 1.0f;
+
+    [Header("Movement Speed")]
+    public float movementSpeed = 2.0f;
+    public float sprintingSpeedFactor = 1.5f;
+    public float crawlingSpeedFactor = 0.5f;
+    public bool canCrawl = false;
+    public bool canSprint = false;
+
+}
