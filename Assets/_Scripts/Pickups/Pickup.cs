2a2c0151fb2ff043b19440baa472e75090394d63
Adam Harris <adam_harris@hotmail.com>
2020-06-22 10:08:18 +0100

    0x43: Added pickup sound effect and particle  effect

--- Assets/_Scripts/Pickups/Pickup.cs
+++ Assets/_Scripts/Pickups/Pickup.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    protected virtual void PickupAction()
-    {
+    [SerializeField] protected AudioClip pickupCollectedSoundEffect;
+    [SerializeField] protected AudioClip pickupSpawnSoundEffect;
 
+    [SerializeField] protected ParticleSystem pickupParticleEffect;
+
+    protected AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = pickupSpawnSoundEffect;
+        audioSource.Play();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -15,7 +25,19 @@ public class Pickup : MonoBehaviour
         {
             PickupAction();
 
-            Destroy(this.gameObject);
+            audioSource.clip = pickupCollectedSoundEffect;
+            audioSource.Play();
+
+            pickupParticleEffect.Play();
+
+            GetComponent<MeshRenderer>().enabled = false;
+
+            gameObject.GetComponentInChildren<Light>().enabled = false;
+
+            Destroy(this.gameObject, 3.0f);
         }
     }
+
+    protected virtual void PickupAction() { }
+
 }
